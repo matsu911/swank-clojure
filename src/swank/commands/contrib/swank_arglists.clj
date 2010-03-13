b6ab5e5b945cef789a4aaf79a4d0ce66ba20f47d
Hugo Duncan <hugo_duncan@yahoo.com>
2010-03-13 09:49:56 -0500

    seperated swank-autodoc.clj and added tests

--- src/swank/commands/contrib/swank_arglists.clj
+++ src/swank/commands/contrib/swank_arglists.clj
@@ -111,26 +111,6 @@
                                          1)
          :else  (message-format fname arglists pos))))))
 
-(defslimefn autodoc
-  "Return a string representing the arglist for the deepest subform in
-RAW-FORM that does have an arglist. The highlighted parameter is
-wrapped in ===> X <===."
-  [raw-specs & options]
-  (let [{:keys [print-right-margin
-                print-lines]} (if (first options)
-                                (apply hash-map options)
-                                {})]
-    (if (and raw-specs
-             (seq? raw-specs))
-      (let [expr (some #(and (seq? %) (some #{:cursor-marker} %) %)
-                       (tree-seq seq? seq raw-specs))]
-        (if (and (seq? expr) (not (= (first expr) "")))
-          ((slime-fn 'operator-arglist)
-           (first expr)
-           *current-package*)
-          `:not-available))
-      `:not-available)))
-
 (defslimefn variable-desc-for-echo-area [variable-name]
   (with-emacs-package
    (or
